431eed4f2bf93fa9148c13a752189d3c426e8ff2
martinoravsky <c8dQIEBuT>
2017-11-27 00:29:34 +0100

    Added deploy_scripts

---
+++ deploy_scripts/install_mptcp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 379CE192D401AB61
+
+echo 'deb https://dl.bintray.com/cpaasch/deb jessie main' >> /etc/apt/sources.list
+
+sudo apt-get update
+sudo apt-get install linux-mptcp
+
+echo "Now configure your grub to use MPTCP kernel and reboot!"
