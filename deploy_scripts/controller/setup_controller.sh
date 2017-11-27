554cd7a858b31d30304af68b0163d3ed729d64a8
martinoravsky <c8dQIEBuT>
2017-11-27 17:49:43 +0100

    Added deploy_scripts

--- deploy_scripts/controller/setup_controller.sh
+++ deploy_scripts/controller/setup_controller.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Setting up Linux MPTCP"
 sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 379CE192D401AB61
 
 echo 'deb https://dl.bintray.com/cpaasch/deb jessie main' >> /etc/apt/sources.list
@@ -7,4 +8,10 @@ echo 'deb https://dl.bintray.com/cpaasch/deb jessie main' >> /etc/apt/sources.li
 sudo apt-get update
 sudo apt-get install linux-mptcp
 
+echo 'Setting up MySQL server'
+sudo apt-get install mariadb-server mariadb-client
+
+echo 'Import MySQL database dump.' 
+mysql --defaults-file=/etc/mysql/debian.cnf -e "create database mptcp;"
+mysql --defaults-file=/etc/mysql/debian.cnf mptcp < mptcp.sql
 echo "Now configure your grub to use MPTCP kernel and reboot!"
