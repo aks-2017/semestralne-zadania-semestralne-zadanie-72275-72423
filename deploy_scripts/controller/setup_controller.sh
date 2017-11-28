41b7355a1e70ec5adfdf372ae07bd1c291e87cee
martinoravsky <c8dQIEBuT>
2017-11-28 09:14:43 +0100

    Added more deploy scripts

--- deploy_scripts/controller/setup_controller.sh
+++ deploy_scripts/controller/setup_controller.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "Setting up Linux MPTCP"
-sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 379CE192D401AB61
+#echo "Setting up Linux MPTCP"
+#sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 379CE192D401AB61
+#
+#echo 'deb https://dl.bintray.com/cpaasch/deb jessie main' >> /etc/apt/sources.list
+#
+#sudo apt-get update
+#sudo apt-get install linux-mptcp
 
-echo 'deb https://dl.bintray.com/cpaasch/deb jessie main' >> /etc/apt/sources.list
-
-sudo apt-get update
-sudo apt-get install linux-mptcp
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
 echo 'Setting up MySQL server'
 sudo apt-get install mariadb-server mariadb-client
@@ -16,6 +18,11 @@ mysql --defaults-file=/etc/mysql/debian.cnf -e "create database mptcp;"
 mysql --defaults-file=/etc/mysql/debian.cnf mptcp < mptcp.sql
 echo "Now configure your grub to use MPTCP kernel and reboot!"
 
+echo 'Installing dependencies.. '
+
+pip install networkx
+pip install mysql-connector
+pip install mysql-connector-python-rf
 #echo "Downloading openvswitch..' 
 #apt-get install build-essentials
 #wget http://openvswitch.org/releases/openvswitch-2.8.1.tar.gz
--- deploy_scripts/host1/setup_host.sh
+++ deploy_scripts/host1/setup_host.sh
@@ -5,7 +5,15 @@ sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 379CE192D401AB61
 
 echo 'deb https://dl.bintray.com/cpaasch/deb jessie main' >> /etc/apt/sources.list
 
+sudo apt-get install apt-transport-https net-tools
 sudo apt-get update
 sudo apt-get install linux-mptcp
 
-echo "Now configure your grub to use MPTCP kernel and reboot!"
+echo "Copying grub config file"
+cp grub /etc/default/
+
+echo "Generating grub config"
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Now reboot!"
+
